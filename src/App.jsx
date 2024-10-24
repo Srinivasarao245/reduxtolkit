be796033fc375c4284577c3a0e86c9a04e8dfcd1
Srinivasarao245 <srinivasaraobagam@gmail.com>
2024-10-24 15:22:57 +0530

    master

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="App bg-warning">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +18,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <h1>$riniva$</h1>
     </div>
   );
 }
